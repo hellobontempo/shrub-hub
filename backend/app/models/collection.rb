90221da1982c6651bb8d2f3eb50841b868bd74bf
claire <clairebontempo@gmail.com>
2021-02-18 16:27:44 -0800

    adds active record validations

--- backend/app/models/collection.rb
+++ backend/app/models/collection.rb
@@ -1,5 +1,7 @@
 class Collection < ApplicationRecord
     has_many :plant_collections, dependent: :destroy
     has_many :plants, through: :plant_collections
-
+    validates :name, presence: true
+    validates :user, presence: true
+    validates_presence_of :plants
 end
--- backend/app/models/plant.rb
+++ backend/app/models/plant.rb
@@ -1,4 +1,8 @@
 class Plant < ApplicationRecord
     has_many :plant_collections, dependent: :destroy
     has_many :collections, through: :plant_collections
+    validates :sci_name, presence: true, uniqueness: true
+    validates :common_name, presence: true
+    validates :care, presence: true
+    validates :img_src, presence: true
 end
