a5ed4c7145d7184b2c35dbf4e749eb25d20c084c
claire <clairebontempo@gmail.com>
2021-02-19 11:12:45 -0800

    edits seed file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -17,8 +17,6 @@ succulents = Plant.create([
     common_name: "Short-leaved Aloe",
     img_src: "https://cdn11.bigcommerce.com/s-oqm1pc/images/stencil/1280x1280/products/2158/3445/aloe-brevifolia-small__62237.1484605662.jpg?c=2"
     }
-    # {sci_name: , img_src: , common_name: , care:}
-    # {sci_name: , img_src: , common_name: , care:}
 ])
 tropics = Plant.create([
     {sci_name: "Philodendron bipinnatifidum",
@@ -43,16 +41,34 @@ tropics = Plant.create([
     }
 ])
 
-Plant.create([
-    {sci_name: "Helleborus niger",
-    care: "Helleborus niger, commonly called Christmas rose or black hellebore, is an evergreen perennial flowering plant in the buttercup family, Ranunculaceae. It is poisonous. Although the flowers resemble wild roses (and despite its common name), Christmas rose does not belong to the rose family (Rosaceae).",
-    common_name: "Christmas rose, Black hellebore",
-    img_src: "https://static.ashwoodnurseries.com/shop/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/l/black_double_resized.jpg"
+easy = Plant.create([   
+    {sci_name: "Monstera deliciosa",
+    care: "Because of its origin in the tropical rainforests of Central and South America, the Swiss cheese plant is best suited to indoor growth in North America, as it cannot withstand temperatures below 55 degrees Fahrenheit. When planting a Monstera, select a deep pot with good drainage, and a peaty potting soil. Fill the bottom third of the pot with soil, and establish a stake in the pot as these plants are climbers. Cover the roots with soil and use plant ties to secure the stem to the stake. Place in a location that receives bright, indirect sunlight. Monsteras do best with moderate watering about once a week during the growing season, and less frequently during the fall and winter.",
+    common_name: "Swiss Cheese ",
+    img_src: "https://smartgardenguide.com/wp-content/uploads/2019/09/monstera-deliciosa-care-2.jpg"
+    },
+    {sci_name: "Maranta leuconeura",
+    care: "Prayer plants are low-growing, spreading plants that thrive best when provided with greenhouse-like conditions, including warm, moist, gentle airflow, and plenty of fertilizer. Plants that are kept too cool or too dry are likely to lose their leaves or suffer from fungal infections that can cause the plant to die from root rot or collapse. Likewise, plants that are exposed to too much sun can become washed out and develop brown blotches on their leaves.",
+    common_name: "Prayer Plant",
+    img_src: "https://www.trees.com/wp-content/uploads/2018/06/Maranta-leuconeura-scaled.jpg"
+    },
+    {sci_name: "Aspidistra elatior",
+    care: " Tolerant of neglect, it is widely cultivated as a houseplant. It is tolerant of low light, low humidity, temperature fluctuation and irregular watering. It is best situated in a position away from direct sun to avoid leaf bleaching. Good drainage is also required for optimal growth and to avoid root rot.",
+    common_name: "Cast Iron Plant",
+    img_src: "https://www.florabundance.com/wp-content/uploads/2018/03/aspidistra-variegated-2.jpg"
     },
     {sci_name: "Zamioculcas",
     care: "Zamioculcas (common names Zanzibar gem, ZZ plant, Zuzu plant, aroid palm, eternity plant or emerald palm) is a genus of flowering plant in the family Araceae, containing the single species Zamioculcas zamiifolia. It is a tropical perennial plant native to eastern Africa, from Kenya south to northeastern South Africa. Zamioculcas is grown as an ornamental plant, mainly for its attractive glossy foliage and easy care. Dutch nurseries started wide-scale commercial propagation of the plant around 1996. It was first described as Caladium zamiifolium by Loddiges in 1829, moved to his new genus Zamioculcas by Heinrich Wilhelm Schott and given its established name Zamioculcas zamiifolia by Adolf Engler.",
     common_name: "Zanzibar gem, Zz plant",
     img_src: "https://www.mydomaine.com/thmb/DIJr_inWdH5MgRYtoeoHUSTR0Uw=/1885x1414/smart/filters:no_upscale()/zz-plant-pot-f34da789c4c14ef7a247ebafcff7e9c3.jpg"
+    }
+])
+
+Plant.create([
+    {sci_name: "Helleborus niger",
+    care: "Helleborus niger, commonly called Christmas rose or black hellebore, is an evergreen perennial flowering plant in the buttercup family, Ranunculaceae. It is poisonous. Although the flowers resemble wild roses (and despite its common name), Christmas rose does not belong to the rose family (Rosaceae).",
+    common_name: "Christmas rose, Black hellebore",
+    img_src: "https://static.ashwoodnurseries.com/shop/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/l/black_double_resized.jpg"
     },
     {sci_name: "Philodendron hederaceum",
     care: "Philodendron hederaceum (common name: heartleaf philodendron synonym: Philodendron scandens) is a species of flowering plant in the family Araceae, native to Central America and the Caribbean.",
@@ -73,9 +89,16 @@ Plant.create([
     care: "Moderate Light Levels Avoid direct sunlight and very shady areas. Moderate Watering Once a week or so in Summer and once every two weeks in Winter. Feeding Try to fertilize once every three months when it's growing.",
     common_name: "Zebra Plant",
     img_src: "https://www.vistasucculents.com/wp-content/uploads/2020/04/Zebra_3-e1587531500273.jpeg"
-}
+    },   
+    {sci_name: "Dionaea muscipula",
+    care: "Regardless of the propagation method used, the plants will live for 20 to 30 years if cultivated in the right conditions. Place in bright indirect sunlight indoors. Keep planting mix very moist at all times; Using distilled water is best. To fertilize it, just feed it insects! If that’s too icky a very mild amount of fertilizer applied directly to the traps will suffice.",
+    common_name: "Venus Fly Trap",
+    img_src: "http://cdn.shopify.com/s/files/1/0038/9405/0868/products/Houseplants_VenusFlytrap_1024x1024.jpg?v=1593291790"
+    }
 ])
+
 Collection.create([
     {name: "Succulents", user: "Claire", plants: tropics},
-    {name: "Tropical", user: "Nicco", plants: succulents}
+    {name: "Tropical", user: "Chrysanthemum", plants: succulents},
+    {name: "Easy Growers", user: "Lily", plants: easy}
 ])
